6dad6316bade7ce6b43ed595f76eec29c44c08d8
GingerBaddie <professionalmahito@gmail.com>
2025-08-27 01:44:17 +0530

    nothing here

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,16 +25,16 @@ products.forEach((product) => {
 
             <div class="product-quantity-container">
                 <select >
-                <option selected value="1" data-product-quantity = "${product.quantity}">1</option>
-                <option value="2" data-product-quantity = "${product.quantity}">2</option>
-                <option value="3" data-product-quantity = "${product.quantity}">3</option>
-                <option value="4" data-product-quantity = "${product.quantity}">4</option>
-                <option value="5" data-product-quantity = "${product.quantity}">5</option>
-                <option value="6"data-product-quantity = "${product.quantity}">6</option>
-                <option value="7"data-product-quantity = "${product.quantity}">7</option>
-                <option value="8"data-product-quantity = "${product.quantity}">8</option>
-                <option value="9"data-product-quantity = "${product.quantity}">9</option>
-                <option value="10"data-product-quantity = "${product.quantity}">10</option>
+                <option selected value="1" >1</option>
+                <option value="2" >2</option>
+                <option value="3" >3</option>
+                <option value="4" >4</option>
+                <option value="5" >5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
                 </select>
             </div>
 
