d6da63cbf96a17001f20d8a98a727a54bce3386a
GingerBaddie <professionalmahito@gmail.com>
2025-08-27 01:43:46 +0530

    cart.js created

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,17 +24,17 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-                <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                <select >
+                <option selected value="1" data-product-quantity = "${product.quantity}">1</option>
+                <option value="2" data-product-quantity = "${product.quantity}">2</option>
+                <option value="3" data-product-quantity = "${product.quantity}">3</option>
+                <option value="4" data-product-quantity = "${product.quantity}">4</option>
+                <option value="5" data-product-quantity = "${product.quantity}">5</option>
+                <option value="6"data-product-quantity = "${product.quantity}">6</option>
+                <option value="7"data-product-quantity = "${product.quantity}">7</option>
+                <option value="8"data-product-quantity = "${product.quantity}">8</option>
+                <option value="9"data-product-quantity = "${product.quantity}">9</option>
+                <option value="10"data-product-quantity = "${product.quantity}">10</option>
                 </select>
             </div>
 
@@ -45,7 +45,8 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id = "${product.id}">
                 Add to Cart
             </button>
             </div>`
@@ -53,4 +54,30 @@ products.forEach((product) => {
         
         
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;        
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+        .forEach((button) => {
+            button.addEventListener('click', () => {
+            const productId = (button.dataset.productId);
+            let matchingItem;
+            cart.forEach((item) => {
+                if (item.productId === productId) {
+                  matchingItem = item;
+            }
+        });
+
+           if (matchingItem) {
+                    matchingItem.quantity += 1;
+                }
+
+                else {
+                     cart.push({
+                productId: productId,
+                quantity: 1
+              }) ;
+                }
+                
+                console.log(cart);
+});
+        });
